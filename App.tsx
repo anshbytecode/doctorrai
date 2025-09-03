7aa553360c61ce2deecf582d859852aeaa30787c
Anshul Bhilare <terminatoranshul@gmail.com>
2025-09-03 23:48:32 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
+import { Toaster } from "@toaster";
+import { Toaster as Sonner } from "@sonner";
+import { TooltipProvider } from "@tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from ".Index";
@@ -27,3 +27,4 @@ const App = () => (
 
 
 export default App;
+
