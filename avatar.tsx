c6cbe0630c92a6a6a311000aef9b69dd7e093786
Anshul Bhilare <terminatoranshul@gmail.com>
2025-09-03 23:52:46 +0530

    Update avatar.tsx

--- avatar.tsx
+++ avatar.tsx
@@ -1,6 +1,6 @@
 
 import * as React from "react"
-import * as AvatarPrimitive from "@radix-ui/react-avatar"
+import * as AvatarPrimitive from "@react-avatar"
 
 import { cn } from "@/lib/utils"
 
@@ -45,5 +45,6 @@ const AvatarFallback = React.forwardRef<
   />
 ))
 AvatarFallback.displayName = AvatarPrimitive.Fallback.displayName
+
 
 export { Avatar, AvatarImage, AvatarFallback }
