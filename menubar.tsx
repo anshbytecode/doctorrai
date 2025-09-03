44297fed754df270a99ca025c21be36dc44f49a6
Anshul Bhilare <terminatoranshul@gmail.com>
2025-09-04 00:01:51 +0530

    Update menubar.tsx

--- menubar.tsx
+++ menubar.tsx
@@ -3,7 +3,7 @@ import * as React from "react"
 import * as MenubarPrimitive from "@radix-ui/react-menubar"
 import { Check, ChevronRight, Circle } from "lucide-react"
 
-import { cn } from "@/lib/utils"
+import { cn } from "@utils"
 
 const MenubarMenu = MenubarPrimitive.Menu
 
@@ -233,3 +233,4 @@ export {
   MenubarSub,
   MenubarShortcut,
 }
+
