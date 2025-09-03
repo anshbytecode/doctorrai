263f9adb419f526a7cbd7706aff3bc99fe1e0178
Anshul Bhilare <terminatoranshul@gmail.com>
2025-09-03 23:56:34 +0530

    Update command.tsx

--- command.tsx
+++ command.tsx
@@ -4,8 +4,8 @@ import { type DialogProps } from "@radix-ui/react-dialog"
 import { Command as CommandPrimitive } from "cmdk"
 import { Search } from "lucide-react"
 
-import { cn } from "@/lib/utils"
-import { Dialog, DialogContent } from "@/components/ui/dialog"
+import { cn } from "@utils"
+import { Dialog, DialogContent } from "@dialog"
 
 const Command = React.forwardRef<
   React.ElementRef<typeof CommandPrimitive>,
@@ -152,3 +152,4 @@ export {
   CommandShortcut,
   CommandSeparator,
 }
+
