50198a9a92986b8691f203e36785965cab78037c
Anshul Bhilare <terminatoranshul@gmail.com>
2025-09-03 23:55:47 +0530

    Update chart.tsx

--- chart.tsx
+++ chart.tsx
@@ -2,7 +2,7 @@
 import * as React from "react"
 import * as RechartsPrimitive from "recharts"
 
-import { cn } from "@/lib/utils"
+import { cn } from "@utils"
 
 // Format: { THEME_NAME: CSS_SELECTOR }
 const THEMES = { light: "", dark: ".dark" } as const
@@ -362,3 +362,4 @@ export {
   ChartLegendContent,
   ChartStyle,
 }
+
