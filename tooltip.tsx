87780e668d94baf68c764058e73da2cab9c1b2a7
Anshul Bhilare <terminatoranshul@gmail.com>
2025-09-04 13:23:37 +0530

    Update tooltip.tsx

--- tooltip.tsx
+++ tooltip.tsx
@@ -2,7 +2,7 @@
 import * as React from "react"
 import * as TooltipPrimitive from "@radix-ui/react-tooltip"
 
-import { cn } from "@/lib/utils"
+import { cn } from "@utils"
 
 const TooltipProvider = TooltipPrimitive.Provider
 
@@ -27,3 +27,4 @@ const TooltipContent = React.forwardRef<
 TooltipContent.displayName = TooltipPrimitive.Content.displayName
 
 export { Tooltip, TooltipTrigger, TooltipContent, TooltipProvider }
+
