6621d840bf167dfe03a3af7e3ea7055c4e354e25
Anshul Bhilare <terminatoranshul@gmail.com>
2025-09-04 13:33:07 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "tagger";
+import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -20,4 +20,5 @@ export default defineConfig(({ mode }) => ({
     },
   },
 }));
+
 
